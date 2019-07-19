dd2b2d15418d18c583f27bd9ab24155aba623c7a
Antonio <lorenzo19anthony@gmail.com>
2019-07-18 22:25:29 -0600

    streamreader

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnProcessFile = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnProcessFile
+            // 
+            this.BtnProcessFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.BtnProcessFile.Location = new System.Drawing.Point(273, 74);
+            this.BtnProcessFile.Name = "BtnProcessFile";
+            this.BtnProcessFile.Size = new System.Drawing.Size(277, 60);
+            this.BtnProcessFile.TabIndex = 0;
+            this.BtnProcessFile.Text = "Process FIle";
+            this.BtnProcessFile.UseVisualStyleBackColor = true;
+            this.BtnProcessFile.Click += new System.EventHandler(this.BtnProcessFile_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(298, 186);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.BtnProcessFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnProcessFile;
+        private System.Windows.Forms.Label lblCount;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO; using System.Threading;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,30 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("C:\\Data\\Data.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length; Thread.Sleep(5000);
+
+
+            }
+
+            return count;
+        }
+
+        private async void BtnProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            lblCount.Text = "Processing file. Please wait...";
+            int count = await task;
+            lblCount.Text = count.ToString() + " characters in file";
+
         }
     }
 }
